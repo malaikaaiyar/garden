53a9bede1d3bb76c346cdef5140e2cabd690ec1a
malaika <51083763+malaikaaiyar@users.noreply.github.com>
2026-03-07 17:11:01 +0000

    hi

---
+++ content/index.md
@@ -0,0 +1,6 @@
+---
+title: welcome to the garden
+---
+
+This is the digital garden of [Malaika](https://malaikaaiyar.me), who will be doing a lot of [learning](https://malaikaaiyar.me/posts/syllabus) over the next few months. This garden will hold her progress. 
+
--- quartz.config.ts
+++ quartz.config.ts
@@ -1,14 +1,9 @@
 import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
-/**
- * Quartz 4 Configuration
- *
- * See https://quartz.jzhao.xyz/configuration for more information.
- */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "working notes",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
@@ -16,39 +11,39 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "malaikaaiyar.me",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Source Code Pro",
+        body: "Source Code Pro",
+        code: "Source Code Pro",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          light: "#FFFDFA",
+          lightgray: "#d4d2cf",
+          gray: "#8a8a8a",
+          darkgray: "#0e3012",
+          dark: "#0e3012",
+          secondary: "#0e3012",
+          tertiary: "#5959FF",
+          highlight: "rgba(14, 48, 18, 0.08)",
+          textHighlight: "rgba(89, 89, 255, 0.2)",
         },
         darkMode: {
-          light: "#161618",
-          lightgray: "#393639",
-          gray: "#646464",
-          darkgray: "#d4d4d4",
-          dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#b3aa0288",
+          light: "#0e3012",
+          lightgray: "#1a4a1e",
+          gray: "#4a7a4e",
+          darkgray: "#e0ddd8",
+          dark: "#FFFDFA",
+          secondary: "#FFFDFA",
+          tertiary: "#5959FF",
+          highlight: "rgba(255, 253, 250, 0.08)",
+          textHighlight: "rgba(89, 89, 255, 0.25)",
         },
       },
     },
@@ -88,10 +83,9 @@ const config: QuartzConfig = {
       Plugin.Static(),
       Plugin.Favicon(),
       Plugin.NotFoundPage(),
-      // Comment out CustomOgImages to speed up build time
       Plugin.CustomOgImages(),
     ],
   },
 }
 
-export default config
+export default config 
